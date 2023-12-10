8f0a9266dd207557abec502af724ddb8b5b82cd6
shashank398@gmail.com <you@example.com>
2023-12-11 00:54:58 +0530

    added models

---
+++ models/Profile.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+
+const profileSchema=new mongoose.Schema({
+    gender:{
+        type:String
+    },
+    dateOfBirth:{
+        type:String
+    },
+    about:{
+        type:String,
+        trim:true
+    },
+    contactNumber:{
+        type:Numbermber,
+        trim:true
+    }
+
+})
+
+module.exports=mongoose.model("Profile",profileSchema)
