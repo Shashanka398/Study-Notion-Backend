add87e08568944704517e67043bd5c0e7cf732a7
shashank398@gmail.com <you@example.com>
2023-12-12 00:31:03 +0530

    updated controllers

--- models/Otp.js
+++ models/Otp.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const mailSender = require("../utils/mailSender");
 const otpSchema=new mongoose.Schema({
     email:{
         type:String,
@@ -15,4 +16,22 @@ const otpSchema=new mongoose.Schema({
     }
 }) 
 
+//to send mail
+async function sendVerification(email,otp){
+    try{
+        const mailResponse= await mailSender(email,"Verification email from studyNotion",otp);
+        console.log("Email Sent Successfully",mailResponse);
+
+    }
+    catch(error){
+        console.log("Error in sendVerification")
+        console.log(error);
+        throw error;
+    }
+}
+
+otpSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
 module.exports=mongoose.model("Otp",otpSchema)
